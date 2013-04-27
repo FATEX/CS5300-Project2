775354207d1f34cc686be19bfbd7529c8687b433
Matthew Green <mcg67@cornell.edu>
2013-04-27 01:22:24 -0400

    Switched Over To A MapReduce Project In Eclipse With The New Plugin

---
+++ PageRankMapReduce/src/project2/LeMapper.java
@@ -0,0 +1,65 @@
+package project2;
+
+import java.io.IOException;
+import java.util.*;
+
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.conf.*;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapreduce.*;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.*;
+
+public class LeMapper extends Mapper<LongWritable, Text, Text, Text> {
+	protected Text capture = new Text();
+
+	protected void map(LongWritable key, Text value, Context context)
+			throws IOException, InterruptedException {
+		
+		//Need A File In The Format "NODE pageRank Degree"
+		
+		String line = value.toString();
+		line = line.trim();
+		System.out.println(line);
+		
+		String[] temp = line.split(" ");
+		System.out.println(temp[0]);
+		System.out.println(temp[1]);
+		//System.out.println(temp[2]); //Blank SPACE!
+		System.out.println(temp[2]);
+		
+		Text Node = new Text(temp[0]);
+		Float pageRank = new Float(temp[1]);
+		Integer degree = new Integer(temp[2]);
+		
+
+		if (degree == 0) {
+			pageRank = (float) 1.0;
+		} else {
+			pageRank = (float) pageRank / degree;
+		}
+		
+		//Concatenate The String For The Reducer To Process With The Outgoing Edges
+		Text edgeList = new Text();
+		String tempList = "";
+
+		for (int i = 1; i < temp.length - 1; i++) {
+			tempList += temp[i] + " ";
+		}
+		
+		tempList += temp[temp.length - 1];
+		edgeList = new Text(tempList);
+
+		context.write(Node, edgeList);
+		cleanup(context);
+		Text nodeOut = new Text();
+		Text pageRankText = new Text(String.valueOf(pageRank));
+
+		for (int i = 3; i < temp.length; i++) {
+			nodeOut = new Text(temp[i]);
+			context.write(nodeOut, pageRankText);
+			cleanup(context);
+		}
+	}
+}
---
+++ PageRankMapReduce/src/project2/LeReducer.java
@@ -0,0 +1,76 @@
+package project2;
+
+import java.io.IOException;
+import java.util.*;
+
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.conf.*;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapreduce.*;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.*;
+
+public class LeReducer extends Reducer<Text, Text, Text, Text> {
+
+	protected void reduce(Text key, Iterable<Text> values, Context context)
+			throws IOException, InterruptedException {
+		
+		Iterator<Text> itr = values.iterator();
+		Text input = new Text();
+		String[] inputTokens = null;
+		
+		Float pageRank = (float) 0.0;
+		Float oldPageRank = (float) 0.0;
+		Float residual = (float) 0.0;
+		Float threshold = (float) 0.01; //Tenth of a percent THIS IS THE CONVERGENCE VALUES
+		
+		ArrayList<String> edgeList = new ArrayList<String>();
+		
+		String output = "";
+
+		while (itr.hasNext()) {
+			input = itr.next();
+			String inputStr = input.toString();
+			inputTokens = inputStr.split(" ");
+			
+			if (inputTokens.length == 1) {
+				pageRank = pageRank + Float.parseFloat(inputTokens[0]);
+			} else {
+				oldPageRank = Float.parseFloat(inputTokens[0]);
+				for (int i = 0; i < inputTokens.length; i++) {
+					edgeList.add(i, new String(inputTokens[i]));
+				}
+			}
+		}
+		
+		if (Float.compare(pageRank, threshold) > 1) {
+			residual = (oldPageRank - pageRank) / pageRank;
+		} else {
+			System.out.println("Error In The Reducer Class. Something went wrong in the PageRank versus Threshold test.");
+		}
+		
+		if (Float.compare(threshold, residual) > 1) {
+			System.out.println("Convergence. Value is less than 0.01 terminal value! Yay!");
+			pageRank = oldPageRank;
+		}
+
+		// TODO: check if this condition is correct
+		if (edgeList.size() > 0) {
+			edgeList.remove(0);
+		}
+		edgeList.add(0, new String(String.valueOf(pageRank)));
+		
+		for (int i = 0; i < edgeList.size() - 1; i++) {
+			output += edgeList.get(i) + "\t";
+		}
+		
+		output += edgeList.get(edgeList.size() - 1);
+
+		Text outputText = new Text(output);
+
+		context.write(key, outputText);
+		cleanup(context);
+	}
+
+}
